ad8210423d8565d03a820614b6e4b45219b52fcb
Mark Kewley <markkewley@workfront.com>
2017-02-18 20:36:18 -0600

    initial commit

---
+++ src/main/java/react/controller/HomeController.java
@@ -0,0 +1,15 @@
+package react.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        // will look for index.html underneath /resources/template
+        return "index";
+    }
+
+}
